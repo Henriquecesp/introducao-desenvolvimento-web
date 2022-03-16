f834b371f3ca22cf913fc6df2856f5281224c838
henrique <henriquecesps4@gmail.com>
2022-03-16 12:10:08 -0300

    arquivos iniciais

--- index.js
+++
@@ -1,9 +0,0 @@
-function getValue() {
-  const value = document.getElementById("name").value;
-  document.getElementById("result").innerHTML = "Bem vindo, " + value + "!";
-}
-
-function removeValue() {
-  document.getElementById("name").value = "";
-  document.getElementById("result").innerHTML = "";
-}
